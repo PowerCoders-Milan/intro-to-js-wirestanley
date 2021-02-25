e28b65fb7e9ae153ab8479e07994eaae66b61541
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-25 15:54:28 +0100

    exercise4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,12 @@
 // Example
 window.alert("Hello World!");
+var name = prompt(" enter your name ")
+alert("hello dear " + name)
+var your_mood_today = prompt(" how is your day dear ")
+
+if (your_mood_today === "bad"){
+    alert("that is too bad tomorrow is a new day dear")
+}
+else if (your_mood_today === "good"){
+    alert("i am happy for you dear")
+}
