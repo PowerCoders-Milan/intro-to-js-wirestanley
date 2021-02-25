3748d6d82cb999fbea53fe0c2bb85bc4cacc4072
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-25 17:22:18 +0100

    exercise5

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,2 +1,19 @@
 // Example
 window.alert("Hello World!");
+var phrase = prompt("can you write a sentence or a word please")
+alert(phrase+ " is " + phrase.length + " number of character ")
+
+// extra....................................................
+alert("can we have 2 different word or phrase one after the other so we can compare")
+var phraseone = prompt("give your first phrase")
+var phrasetwo = prompt("give your second phrase ")
+
+if(phraseone.length > phrasetwo){
+    alert(phraseone + "phrase was the longest with " + phraseone.length + "number of charcters.")
+}
+else if(phrasetwo.length > phraseone.length){
+    alert(phrasetwo + " phrase was the longest with " +phrasetwo.length + " number of characters. ")
+}
+else{
+    alert(" both phrase have equal number of character")
+}
