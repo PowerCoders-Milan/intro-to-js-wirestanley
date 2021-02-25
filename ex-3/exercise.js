7df86b66b5e6816ed6038b50ad68ff0bc9afd2af
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-25 15:08:03 +0100

    exercise3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,14 @@
 // Example
 window.alert("Hello World!");
+var x= 10
+var y= 10
+if (x > y) {
+    window.alert(x + " is greater than " + y);
+
+}
+else if (y > x) {
+    window.alert(y +" is greater than " + x);
+}
+else { window.alert(" the numbers are thesame ");
+
+}
