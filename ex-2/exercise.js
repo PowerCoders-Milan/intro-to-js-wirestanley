daf45bf13a7e0d1f6d7cfaa92dc6491fed8227ea
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-25 14:42:02 +0100

    exercise2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,18 @@
 // Example
 window.alert("Hello World!");
+var birthday = prompt("Can I have your birth year", "0");  
+var futureYear = prompt("Can I have the future year you want me to calculate", "0")
+var newAge = futureYear - birthday;
+
+window.alert("You will be either " +newAge+ " or " +(newAge-1)+ " in " +futureYear+ ".")
+
+
+// Extra....
+var birthyear = prompt("Can i have your birth Year", "0");
+var birthmonth = prompt("can i have the number of the birth month", "0");
+var birthday = prompt("Can I have the number of the day your were born", "0");
+var actualyear = 2021;
+var actualmonth = 12;
+var actualday = 25;
+
+window.alert("You are " +(actualyear-birthyear)+ " years " +(actualmonth-birthmonth)+ " months and " +(actualday-birthday)+ " days old.") 
