213cc8ef813078b99dd01ba5e230273e62116878
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-25 12:12:46 +0100

    exercise1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,9 @@
 // Example
 window.alert("Hello World!");
+var children = 2;
+var man =" merit";
+var woman =" katlyne";
+var place =" venice";
+var job =" banker";
+
+console.log( "you will be a"+ job + " in " +place+ " and get married to " +woman+ " with " +children+ " kids ")
